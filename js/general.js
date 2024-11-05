d424a7669b83f19175c528b262a0777c32908d7f
Rakultur <Rakultur@hotmail.com>
2024-11-05 11:04:01 -0500

    Modificaciones

--- js/general.js
+++ js/general.js
@@ -5,29 +5,3 @@ const navList = document.querySelector('.nav-list');
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
-
-// Slider imagenes
-let index = 0;
-
-const items = document.querySelectorAll('.carousel-item');
-const totalItems = items.length;
-
-document.getElementById('nextBtn').addEventListener('click', () => {
-    items[index].classList.remove('active');
-    index = (index + 1) % totalItems;
-    items[index].classList.add('active');
-    updateCarousel();
-});
-
-document.getElementById('prevBtn').addEventListener('click', () => {
-    items[index].classList.remove('active');
-    index = (index - 1 + totalItems) % totalItems;
-    items[index].classList.add('active');
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const offset = -index * 100; // Mueve el contenedor según el índice actual
-    carouselContainer.style.transform = `translateX(${offset}%)`;
-}
